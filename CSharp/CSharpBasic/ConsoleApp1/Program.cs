a4a89eb53e70e5fe4d43bf090ae364ef88dd0464
Denbergg <124432934+ZEDP1@users.noreply.github.com>
2023-03-17 18:12:42 +0900

    Variable Update
    - Variable Update

--- CSharp/CSharpBasic/ConsoleApp1/Program.cs
+++ CSharp/CSharpBasic/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 변수
+
+// 변수 선언
+// 자료형 변수이름
+// 변수를 선언한다는 것 : 메모리상에 자료형 크기만큼의 공간을 할당함.
+// 변수 초기화 : 변수 선언 시 대입연산자로 초기화할 값을 대입해줌.
+int a = 8; // 4byte 부호가 있는 정수형
+short short1 = 1332; // 2byte 부호가 있는 정수형
+long long1 = 1321; // 8byte 부호가 있는 정수형
+uint uint1 = 1; // 자료형 앞에 unsigned를 의미하는 u를 붙여 양수만 있는 정수형을 만들 수 있다.
+float hight = 22.4f; // 4byte 실수형
+double weight = 42.1; // 8byte 실수형
+char gneder = 'A'; // 문자형 : 아스키코드를 따르며 2byte 정수형이다. 예) 대문자 A는 65로 표현됨. 65 = 2^6 *1 + 2^0 * 1 (=> 64 + 1를 뜻함.)
+string name = "Luke"; // 문자열형 : 문자 하나당 2byte + 마지막에 null 문자 ( C계통의 언어는 문자열의 끝을 인식하기 위해서 null을 쓴다. )
